54d2708ed8ee62135d3c08733f130835108f5f54
mjarek <mjarek1313@gmail.com>
2018-02-13 11:38:29 +0100

    First commit

---
+++ SalaryManagerApp/SalaryManager/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new CurrencyWS.CurrencyConverterClient();
+            client.GetPolishEuroRate();
+        }
+    }
+}
---
+++ SalaryManagerApp/SalaryManagerApp/ICurrencyConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SalaryManagerApp
+{
+   
+    [ServiceContract]
+    public interface ICurrencyConverter
+    {
+
+        [OperationContract]
+        double GetPolishEuroRate();
+
+        [OperationContract]
+        double GetDanishEuroRate();
+
+        
+    }
+
+    
+}
